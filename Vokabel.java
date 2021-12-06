4ec96374b81b26ce3cbb48e8368dd31dc07f6de0
coinstore <75029607+EHubert04@users.noreply.github.com>
2021-12-06 09:47:18 +0100

    Vokabel

---
+++ Vokabel.java
@@ -0,0 +1,9 @@
+public class Vokabel {
+    String fremd;
+    String deutsch;
+
+    public Vokabel(String pFremd, String pDeutsch){
+        String fremd = pFremd;
+        String deutsch = pDeutsch;
+    }
+}
