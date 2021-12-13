d5a6616bd44faf2b7f16ed481c23700cfc60dc25
SOhrendorf <74784123+SOhrendorf@users.noreply.github.com>
2021-12-13 14:33:18 +0100

    add methode abfrage() & loeschen()

--- Vokabeltrainer.java
+++ Vokabeltrainer.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Vokabeltrainer {
 
     static List<Vokabel> vokabelListe;
+    int richtig; 
+    int falsch; 
 
     public Vokabeltrainer(){
         //Konstruktor Vokabeltrainer; nötig um static aus main aufzulösen
@@ -21,6 +23,8 @@ public class Vokabeltrainer {
     void menue(){
         //Auswahlmöglichkeiten ausgeben
         System.out.println("1) neue Vokabel einfügen");
+        System.out.println("2) Abfrage starten");
+        System.out.println("3) Vokabel löschen");
         System.out.println("9) alle Vokabeln anzeigen");
         System.out.println("42) Testmethode aufrufen");
         Scanner input = new Scanner(System.in);
@@ -30,6 +34,13 @@ public class Vokabeltrainer {
         switch(auswahl){
             case 1:
                 vokabelnEinfuegen();
+                break;
+
+            case 2:
+                abfrage();
+                break;
+            case 3:
+                spezVokabelloeschen();
                 break;
             case 9:
                 alleVokabel();
@@ -69,6 +80,37 @@ public class Vokabeltrainer {
             System.out.println(vokabelListe.getContent().deutsch + " - " + vokabelListe.getContent().fremd);
             vokabelListe.next();
         }
+        menue();
+    }
+
+    void spezVokabelloeschen(){
+        System.out.println("Welche Vokalebn wollen Sie löschen [Deutschevokabel eingenen]");
+        Scanner input = new Scanner(System.in);
+        String search = input.nextLine();
+        vokabelListe.toFirst();
+        while(!vokabelListe.getContent().deutsch.equals(search)){
+            vokabelListe.next();
+        }
+        vokabelListe.remove();
+        menue();
+    }
+
+    void abfrage(){
+        vokabelListe.toFirst();
+        while(vokabelListe.hasAccess()){
+            System.out.println(vokabelListe.getContent().deutsch);
+            Scanner vokabelinput = new Scanner(System.in);
+            String vokabel = vokabelinput.nextLine();
+            String ov = vokabelListe.getContent().fremd;
+            if(vokabel.equals(ov)){
+                richtig++;
+            }else{
+                falsch++;
+            }
+            vokabelListe.next();;
+        }
+        System.out.println("Du hattest " + richtig  + " Vokabeln richtig");
+        System.out.println("Du hattest " + falsch  + " Vokabeln falsch");
         menue();
     }
 }
